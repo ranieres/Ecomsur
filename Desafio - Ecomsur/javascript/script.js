40753cd31c733c291b912bd3acc1dbd8842037f3
ranieres <74559353+ranieres@users.noreply.github.com>
2020-11-28 03:17:37 -0300

    Add files via upload

---
+++ Desafio - Ecomsur/javascript/script.js
@@ -0,0 +1,8 @@
+function accion(){
+    var ancla = document.getElementsByClassName('nav-enlace');
+
+    for (var i = 0; i < ancla.length; i++) {
+        ancla[i].classList.toggle('esconder');
+    }
+}
+
